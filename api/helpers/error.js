e778d4d740502865f6abfdd15da77e780b79b517
Anni Piragauta <aapiragautau@unal.edu.co>
2018-09-28 16:50:30 -0500

    Adding all the posible error list to handle by error helper
    	I've completed the errors list on helper error located on
    	helpers/error.js, and I've added errors: 400, 401, 409,
    	502, 503 and 504.

--- api/helpers/error.js
+++ api/helpers/error.js
@@ -1,7 +1,17 @@
 const errors = {
-  FORBIDDEN: {error: 'Forbidden', msg: 'You are not allowd to perform this operation', code: 403},
+  BAD_REQUEST: {error: 'Bad request', msg: 'The request could not be understood by the server due to malformed syntax.',
+      code: 400},
+  UNAUTHORIZED: {error: 'Unauthorized', msg: 'Missing or invalid authentication token.', code: 401},
+  FORBIDDEN: {error: 'Forbidden', msg: 'You are not allowed to perform this operation', code: 403},
   NOT_FOUND: {error: 'Not found', msg: 'Resource not found', code: 404},
-  INTERNAL_SERVER_ERROR: {error: 'Internal server error', msg: 'The server encountered an unexpected condition', code: 500}
+  CONFLICT: {error: 'Conflict', msg: 'Request could not be processed because of conflict in the request', code: 409},
+  INTERNAL_SERVER_ERROR: {error: 'Internal server error', msg: 'The server encountered an unexpected condition',
+      code: 500},
+  BAD_GATEWAY: {error: 'Bad gateway', msg: 'While acting as a gateway or proxy, received an invalid response from the' +
+          'upstream server', code: 502},
+  SERVICE_UNAVAILABLE: {error: 'Service Unavailable', msg: 'The server is currently unavailable', code: 503},
+  GATEWAY_TIMEOUT: {error: 'Gateway Timeout', msg: 'While acting as a gateway or proxy, did not receive a timely' +
+          'response from the upstream server', code: 504}
 }
 
 function error (code) {
